ddc5284ffc6ded46be96374666014510f4d13561
jlopez <30164447+jorneycr@users.noreply.github.com>
2026-02-17 15:08:06 -0600

    upload project

---
+++ src/test/java/com/example/exporecalijpa/ExporecaliJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exporecalijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExporecaliJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
