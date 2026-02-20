f9545031c562b693219951b2233490aabe4e9bb1
jlopez <30164447+jorneycr@users.noreply.github.com>
2026-02-19 18:07:16 -0600

    added rating feature

--- src/main/java/com/example/exporecalijpa/repository/TourPackageRepository.java
+++ src/main/java/com/example/exporecalijpa/repository/TourPackageRepository.java
@@ -2,9 +2,11 @@ package com.example.exporecalijpa.repository;
 
 import com.example.exporecalijpa.model.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(path = "packages", collectionResourceRel = "packages")
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
 
     Optional<TourPackage> findByName(String name);
